112e7f6c6dbaff37316f1f2ace85e519df3b9b2e
Joe D'Amore <joe@joedamore.me>
2026-07-23 11:48:22 -0400

    Fix script path

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,8 +2,11 @@
 
 set -ouex pipefail
 
+ROOT_DIRECTORY="/ctx/"
+
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
 ### Install packages
-./01_base/01_install-packages.sh
+"$ROOT_DIRECTORY/01_base/01_install-packages.sh"
+
